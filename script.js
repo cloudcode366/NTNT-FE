333043266e85f17b0f3ef664a2d5d6d2f970fe16
Wiramin <113937901+cloudcode366@users.noreply.github.com>
2025-02-25 09:14:45 +0700

    add content

--- script.js
+++ script.js
@@ -2,6 +2,34 @@
 
 function showPopup(index) {
     const popup = document.getElementById('popup');
+    const popupTitle = document.getElementById('popup-title');
+    const popupContent = document.getElementById('popup-content');
+
+    // Nội dung cho từng phần tử
+    const titles = [
+        "Vào lớp",
+        "Tiết 1: Âm sắc cổ",
+        "Tiết 2: Âm sắc giao",
+        "Tiết 3: Lớp học Dự giờ",
+        "Tiết 4: Trải nghiệm Thực tế",
+        "Tiết 5: Giờ Thực hành",
+        "Tan học"
+    ];
+
+    const contents = [
+        "Hãy trải nghiệm đầy đủ các hoạt động để tích góp được 5 điểm 10 trong lớp học này nhé",
+        "Hoạt động Trải nghiệm Nhạc cụ Truyền thống: Người tham gia được \"nhập môn\" làm học trò của Lão Tre, trực tiếp chạm vào di sản âm nhạc dân tộc, lắng nghe và cảm nhận sự tinh tế trong âm hưởng độc đáo của văn hóa Việt.",
+        "Hoạt động Trải nghiệm và Bình chọn các loại Beat Giao thoa: Nơi học viên được trực tiếp nghe và bình chọn bản phối kết hợp giữa nhạc cụ truyền thống và nhạc điện tử hiện đại, qua đó khám phá và hiểu hơn về phong cách âm nhạc mang đậm dấu ấn giao thoa.",
+        " Talkshow “Tre Truyền Trẻ”: Buổi dự giờ đặc biệt, là dịp để học viên được lắng nghe và khám phá tiềm năng bản thân, gợi mở tư duy về bảo tồn văn hóa thông qua hình thức giao thoa, với sự đồng hành của các Diễn giả: Tiến sĩ Nguyễn Thị Quốc Minh - Giảng viên Khoa Văn học trường Đại học Khoa học Xã hội và Nhân văn, ĐHQG TPHCM; Cô Phạm Thị Kim Long - Giảng viên Bộ môn Nhạc cụ dân tộc trường Đại học FPT; Rapper Rtee và Producer Hổ.",
+        "Hoạt động Thị phạm Sản xuất âm nhạc: Học viên được trực tiếp chứng kiến quá trình sáng tạo âm nhạc, từ lúc lên ý tưởng đến khi hoàn thiện, qua phần thị phạm từ các chuyên gia.",
+        "Trải nghiệm Đọc rap & Gieo vần từ Ca dao tục ngữ: Giờ thực hành đầy cảm hứng, nơi học viên tự tin thử sức với Rap kết hợp ca dao, tục ngữ, thể hiện sự giao thoa độc đáo giữa chất liệu dân gian và phong cách hiện đại.",
+        "Chúc mừng!Bạn đã nhận được 5 điểm 10, xuất xắc hoàn thành buổi học ngày hôm nay"
+    ];
+
+    // Thay đổi nội dung của pop-up
+    popupTitle.textContent = titles[index];
+    popupContent.textContent = contents[index];
+
     popup.classList.remove('inactive');
     popup.classList.add('active');
 
